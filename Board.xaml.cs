3f7d8e2bef4f96a48353ebcbbf89c1e7a68f6478
AGersonNEU <98633255+AGersonNEU@users.noreply.github.com>
2023-07-20 14:14:35 -0600

    start of the setup for main menu and connecting play button to board wpf

--- Board.xaml.cs
+++ Board.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PenteGame
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Board.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Board : Window
     {
-        public MainWindow()
+        public Board()
         {
             InitializeComponent();
-        }
-
-        private void OpenBoard(object sender, RoutedEventArgs e)
-        {
-            this.Hide();
-            Board board = new Board();
-            board.Show();
         }
     }
 }
