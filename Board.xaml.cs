26ac4d551737ade03fce961e71a15cb167263422
AGersonNEU <98633255+AGersonNEU@users.noreply.github.com>
2023-07-24 18:50:10 -0600

    getting the board set up and clickevent for PlacePiece and IsMoveValid set up

--- Board.xaml.cs
+++ Board.xaml.cs
@@ -19,9 +19,44 @@ namespace PenteGame
     /// </summary>
     public partial class Board : Window
     {
+        public static String playerTurn = GlobalVariables.playerOne.Name;
         public Board()
         {
             InitializeComponent();
+        }
+
+        private void MainMenu(object sender, RoutedEventArgs e)
+        {
+            
+            MainWindow menu = new MainWindow();
+            this.Close();
+            menu.Show();
+        }
+
+        private void PlacePiece(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (playerTurn == GlobalVariables.playerOne.Name)
+            {
+                //make the image on the button clicked white
+
+            }else if (playerTurn == GlobalVariables.playerTwo.Name)
+            {
+                //make the image on the button clicked black
+            }
+
+            //making the image seen for the button but they can't click on this button again
+            button.Visibility = Visibility.Visible;
+            button.IsEnabled = false;
+
+            //save method to something
+            IsMoveValid();
+        }
+
+        public bool IsMoveValid()
+        {
+            //check direction for this button move
+            return true;
         }
     }
 }
