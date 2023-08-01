c0ff522de3cfb676a374a460eec4fb1f48dec022
AGersonNEU <98633255+AGersonNEU@users.noreply.github.com>
2023-07-31 23:28:03 -0600

    all buttons have been put on the board and the base for the PlacePiece method has been set up

--- Board.xaml.cs
+++ Board.xaml.cs
@@ -20,9 +20,44 @@ namespace PenteGame
     public partial class Board : Window
     {
         public static String playerTurn = GlobalVariables.playerOne.Name;
+
+        
+        
         public Board()
         {
             InitializeComponent();
+            //J10 is center of Board
+            Button A19 = (Button)BoardOfButtons.FindName("A19");
+            A19.Opacity = 0;
+            A19.IsEnabled = true;
+
+            Button A18 = (Button)BoardOfButtons.FindName("A18");
+            A18.Opacity = 0;
+            A18.IsEnabled = true;
+
+            Button A17 = (Button)BoardOfButtons.FindName("A17");
+            A17.Opacity = 0;
+            A17.IsEnabled = true;
+
+            Button A16 = (Button)BoardOfButtons.FindName("A16");
+            A16.Opacity = 0;
+            A16.IsEnabled = true;
+
+            Button A15 = (Button)BoardOfButtons.FindName("A15");
+            A15.Opacity = 0;
+            A15.IsEnabled = true;
+
+            Button A14 = (Button)BoardOfButtons.FindName("A14");
+            A14.Opacity = 0;
+            A14.IsEnabled = true;
+
+            Button A13 = (Button)BoardOfButtons.FindName("A13");
+            A13.Opacity = 0;
+            A13.IsEnabled = true;
+
+            Button A12 = (Button)BoardOfButtons.FindName("A12");
+            A12.Opacity = 0;
+            A12.IsEnabled = true;
         }
 
         private void MainMenu(object sender, RoutedEventArgs e)
@@ -36,19 +71,25 @@ namespace PenteGame
         private void PlacePiece(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
+            button.Opacity = 100;
             if (playerTurn == GlobalVariables.playerOne.Name)
             {
                 //make the image on the button clicked white
+                
+                button.Background = Brushes.Black;
+                playerTurn = GlobalVariables.playerOne.Name;
 
             }else if (playerTurn == GlobalVariables.playerTwo.Name)
             {
                 //make the image on the button clicked black
+               button.Background = Brushes.White;
+                playerTurn = GlobalVariables.playerOne.Name;
             }
 
             //making the image seen for the button but they can't click on this button again
-            button.Visibility = Visibility.Visible;
+            
             button.IsEnabled = false;
-
+            
             //save method to something
             IsMoveValid();
         }
